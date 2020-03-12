086c74dcdef78cd9005d35f6f1948053f2200df1
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-12 18:53:59 -0400

    about page styled

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,16 +5,16 @@ const About = () => {
   return (
     <section className="about">
       <div className="about-container">
-        <h1>Hi, my friends call me Tig. Nice to meet you.</h1>
-        <h2></h2>
-        <div></div>
-        <h3>
+        <h1 className="about-title">
+          Hi, my friends call me Tig. Nice to meet you.
+        </h1>
+        <h2 className="about-text">
           Since graduating with a degree in finance, the three questions I
           repeatedly ask myself -- Who are you? Where are you going? & Why? --
           helped guide me to realize my goal of building great products instead
           of simply consuming them. After a year of honing my skills, I'm
           determined and ready to help you build great products together.
-        </h3>
+        </h2>
       </div>
     </section>
   );
