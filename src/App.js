1c0cfa7f6c1467eb48623d3607787f6b5e8ef625
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-15 01:00:46 -0400

    complete home view

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Switch>
       <Route exact path="/">
-        <div className="App">
+        <div className="App" id="home">
           <Navbar />
           <Home />
           <Footer />
